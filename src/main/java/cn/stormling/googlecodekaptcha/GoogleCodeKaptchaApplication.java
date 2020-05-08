5429936ee110e5477cf84081a12a3ac8093db5b8
lingzhexi <lingzhxi0@gmail.com>
2020-05-08 09:03:04 +0800

    Kaptcha验证码Demo初始化

---
+++ src/main/java/cn/stormling/googlecodekaptcha/GoogleCodeKaptchaApplication.java
@@ -0,0 +1,17 @@
+package cn.stormling.googlecodekaptcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: StormLing
+ * @Description:
+ * @Date: Created in 9:00 2020-05-08
+ * @Modified By:
+ */
+@SpringBootApplication
+public class GoogleCodeKaptchaApplication {
+    public static void main(String[] args) {
+            SpringApplication.run(GoogleCodeKaptchaApplication.class, args);
+        }
+}
